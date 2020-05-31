b5dc7ad46940b5cd1f666882344465dff28d97c5
alexcostigan <alexcostigan@icloud.com>
2020-05-31 18:53:03 +0100

    nav bar changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,15 +5,15 @@ import './navbar.css'
 const Navbar = () => {
     return (
         <div class="nav">
-            <div class="tc tile">Profile</div>
-            <div class="tc tile">Favorites</div>
+            <div class="tc tile">About</div>
+            <div class="tc tile">Portfolio</div>
             <div class="tc center-tile f2">
                 <a href="" class="fab">
                     <span><img src={logo} /></span>
                 </a>
             </div>
-            <div class="tc tile">Orders</div>
-            <div class="tc tile">Bag</div>
+            <div class="tc tile">Experience</div>
+            <div class="tc tile">Contact Me!</div>
         </div>
     )
 }
